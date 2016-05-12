9b7e1390b1106e26eaee565fa98eb12b3775bf0e
cryptickid <cryptickid@gmail.com>
2016-05-12 09:10:58 -0400

    bring base up to date

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -933,8 +933,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     R.id.header_debug_info);
             mNotificationPanelDebugText.setVisibility(View.VISIBLE);
         }
-        
-        addAppCircleSidebar();
 
         try {
             boolean showNav = mWindowManagerService.hasNavigationBar();
@@ -1502,9 +1500,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 shadeEntry.notifyFullScreenIntentLaunched();
                 MetricsLogger.count(mContext, "note_fullscreen", 1);
             } catch (PendingIntent.CanceledException e) {
-			}
-		}
-            // usual case: status bar visible & not immersive
+            }
+        }
         addNotificationViews(shadeEntry, ranking);
         // Recalculate the position of the sliding windows and the titles.
         setAreThereNotifications();
